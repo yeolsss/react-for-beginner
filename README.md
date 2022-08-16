81af8800fd2d73bdbd91f52bf7008530b666e652
yeol <leesc1313@gmail.com>
2022-08-16 15:11:55 +0900

    2. coin tracker

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React-for-beginner
 
 ### `1. todo list`
+
+### `2. coin tracker`
--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selected, setSelected] = useState('');
+  const [useMoney, setUseMoney] = useState(0);
+  const [buyCoin, setBuyCoin] = useState(0);
+  const reset = () => {
+    setUseMoney(0);
+    setSelected('');
+    handleBuyCoin(useMoney, selected);
   };
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  const handleSelect = (event) => {
+    setSelected(event.target.value);
+    handleBuyCoin(useMoney, event.target.value);
+  };
+
+  const handleDeposit = (event) => {
+    setUseMoney(parseInt(event.target.value));
+    handleBuyCoin(parseInt(event.target.value), selected);
+  };
+  const handleBuyCoin = (eventMoney, eventCoin) => {
+    setBuyCoin(
+      !isNaN(eventMoney / parseFloat(eventCoin))
+        ? eventMoney / parseFloat(eventCoin)
+        : 0
+    );
+  };
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type='text'
-          placeholder='Write your to do...'
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+      <h1>The coins!({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <label htmlFor='useMoney'>사용금액: </label>
+      <input
+        type='number'
+        id='useMoney'
+        value={useMoney}
+        onChange={handleDeposit}
+      />
+      <select onChange={handleSelect} value={selected}>
+        <option value='xx'>select Coin</option>
+        {coins.map((coin) => (
+          <option value={coin.quotes.USD.price}>
+            {coin.name} ({coin.symbol}):${coin.quotes.USD.price}
+          </option>
         ))}
-      </ul>
+      </select>
+      <h1>{buyCoin}개의 Coin을 구매하실수 있습니다.</h1>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
